518f03fb35ce0e1672127d358b3127210f3bd22a
Teerapong Manakul <imumi@msn.com>
2016-10-13 00:44:59 +0700

    shorten function

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,17 +1,43 @@
 'use strict';
 
+var shortid = require('shortid');
 var ClickHandler = require(process.cwd() + '/app/controllers/clickHandler.server.js');
 
 module.exports = function (app, db) {
-   var clickHandler = new ClickHandler(db);
+    var clickHandler = new ClickHandler(db);
 
-   app.route('/')
-      .get(function (req, res) {
-         res.sendFile(process.cwd() + '/public/index.html');
-      });
+    app.route('/')
+    .get(function (req, res) {
+        res.sendFile(process.cwd() + '/public/index.html');
+    });
 
-   app.route('/api/clicks')
-      .get(clickHandler.getClicks)
-      .post(clickHandler.addClick)
-      .delete(clickHandler.resetClicks);
+    app.route('/api/clicks')
+    .get(clickHandler.getClicks)
+    .post(clickHandler.addClick)
+    .delete(clickHandler.resetClicks);
+
+    app.route(/^\/new\/(.+)$/).get(function(req, res) {
+        var original_url = req.originalUrl.replace(/^\/new\/(.+)$/, '$1');
+        var shorten = shortid.generate();
+        var url = {
+            original_url: original_url,
+            shorten: shorten
+        }
+        db.collection('urls').insert(url, function(err, data) {
+            if (err) throw err
+            res.json({
+                original_url: url.original_url,
+                shorten: app.set('app_url') + '/' + url.shorten
+            });
+        });
+    });
+
+    app.route('/:shorten').get(function(req, res) {
+        db.collection('urls').findOne({
+            shorten: req.params.shorten
+        }, function(err, url) {
+            if (err) throw err
+            res.redirect(url.original_url)
+        })
+    });
 };
--- server.js
+++ server.js
@@ -6,12 +6,19 @@ var routes = require('./app/routes/index.js');
 
 var app = express();
 
-mongo.connect('mongodb://localhost:27017/clementinejs', function (err, db) {
+require('dotenv').config()
+
+var url = process.env.MONGODB || 'mongodb://localhost:27017/clementinejs';
+var port = process.env.PORT || 3000;
+
+app.set('app_url', process.env.APP_URL);
+
+mongo.connect(url, function (err, db) {
 
    if (err) {
       throw new Error('Database failed to connect!');
    } else {
-      console.log('Successfully connected to MongoDB on port 27017.');
+      console.log('Successfully connected to ' + url);
    }
 
    app.use('/public', express.static(process.cwd() + '/public'));
@@ -19,8 +26,8 @@ mongo.connect('mongodb://localhost:27017/clementinejs', function (err, db) {
 
    routes(app, db);
 
-   app.listen(3000, function () {
-      console.log('Node.js listening on port 3000...');
+   app.listen(port, function () {
+      console.log('Node.js listening on port ' + port);
    });
 
 });
